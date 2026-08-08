2e2ce44d3e473305a9e8df8280bfb7c005da4c45
Carlo <carlosfevernova@hotmail.com>
2026-08-07 19:59:14 -0600

    fix: excluir /admin del intl matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -43,5 +43,5 @@ export async function middleware(req: NextRequest){
 }
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: ['/((?!api|_next|_vercel|admin|offline|.*\\..*).*)']
 };
